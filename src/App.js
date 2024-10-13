cc1b3455efca5fa34f55411361da90c70e52b9c6
AashishBarme <aashish.barme9@gmail.com>
2024-10-13 22:59:21 +0545

    updated the content and added google analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello There, It's me Aashish Barme.</h2>
         <p>
-          Hello There, K Bye!!
+          Guy who loves Christ, Coffee and Code.
         </p>
+        <p><span><a className='social-icon' href='https://www.facebook.com/AashishJacobBarme'>
+        <i class="fa fa-facebook-official"></i>
+          </a></span>
+          <span><a className='social-icon' href='https://github.com/AashishBarme'>
+        <i class="fa fa-github-square"></i>
+          </a></span>
+          <span><a className='social-icon' href='https://www.linkedin.com/in/aashish-barme'>
+        <i class="fa fa-linkedin-square"></i>
+          </a></span></p>
       </header>
     </div>
   );
